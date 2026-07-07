48b60d219ffe1574621af71a4e8997c05d4aa0f6
HuynhKhoi2711 <huynhanhkhoi9a7@gmail.com>
2026-07-07 15:53:34 +0700

    Finish deploy image

--- webapp/backend/deploy.sh
+++ webapp/backend/deploy.sh
@@ -25,8 +25,16 @@ REPO_URI=$(jq -r '
 ' ../../infrastructure/outputs.json)
 
 # In PC
+IMAGE="$REPO_URI/$REPO_NAME:$ImageName"
 export KUBECONFIG=~/.kube/ec2.yaml
 kubectl get nodes
 
-sed "s|{{image}}|$IMAGE|g" k8s/deployment.yaml | cat k8s/deployment.yaml 
+PASSWORD=$(aws ecr get-login-password --region $Region)
+kubectl create secret docker-registry ecr-secret \
+    --docker-server=$REPO_URI \
+    --docker-username=AWS \
+    --docker-password="$PASSWORD"
 
+sed "s|{{image}}|$IMAGE|g" k8s/deployment.yaml | kubectl apply -f -
+
+kubectl rollout restart deployment hello-world-api
